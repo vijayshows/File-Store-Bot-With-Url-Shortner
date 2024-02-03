4c056f53285fd6a5bae206bcc676d173ea5c36c6
mrhyper178 <153428922+mrhyper178@users.noreply.github.com>
2024-02-03 10:22:20 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,10 +4,10 @@ class Config(object):
   API_ID = int(os.environ.get("API_ID", "8733404"))
   API_HASH = os.environ.get("API_HASH", "f19aed00b0c74abed0359016afc1733f")
 
-  #BOT_TOKEN = os.environ.get("BOT_TOKEN", "6898447547:AAFj3fAep2W2O69lilL3jv-TNluMJE9EnyI")
-  #BOT_USERNAME = os.environ.get("BOT_USERNAME", "QL_Movie_Links_Bot")
-  #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002087036746"))
-  #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001679142047")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6898447547:AAFj3fAep2W2O69lilL3jv-TNluMJE9EnyI")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "QL_Movie_Links_Bot")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002087036746"))
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001679142047")
   
   #BOT_TOKEN = os.environ.get("BOT_TOKEN", "5925688295:AAFjVjgfg_m1f4Wd6B3AaqBAm8vFkH31guk")
   #BOT_USERNAME = os.environ.get("BOT_USERNAME", "Tamilan_Rocks1_Bot")
@@ -24,14 +24,14 @@ class Config(object):
   #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002035253761"))
   #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002028128566")
 
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "5780282761:AAG9vcgAVK__K1btiy212Ao-9E_rlpelADw")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Vijay_Tv_SerialsBot")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002117688872"))
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001728131839")
+  #BOT_TOKEN = os.environ.get("BOT_TOKEN", "5780282761:AAG9vcgAVK__K1btiy212Ao-9E_rlpelADw")
+  #BOT_USERNAME = os.environ.get("BOT_USERNAME", "Vijay_Tv_SerialsBot")
+  #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002117688872"))
+  #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001728131839")
 
   #DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002117688872")) #Vijay_Tv_SerialsBot
-  #DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002087036746")) #QL_Movie_Links_Bot
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002038837206")) #Common
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002087036746")) #QL_Movie_Links_Bot
+  #DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002038837206")) #Common
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "tnshort.net")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "ae8999b1a230fee55d762c4682e14321d29f7038")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "807374433"))
