2f80537c936dfb3ac2ece264f857651aa0d0d88a
mrhyper178 <153428922+mrhyper178@users.noreply.github.com>
2024-02-09 16:11:36 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,15 +9,15 @@ class Config(object):
   #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002087036746"))
   #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001679142047")
   
-  #BOT_TOKEN = os.environ.get("BOT_TOKEN", "5925688295:AAFjVjgfg_m1f4Wd6B3AaqBAm8vFkH31guk")
-  #BOT_USERNAME = os.environ.get("BOT_USERNAME", "Tamilan_Rocks1_Bot")
-  #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001884285982"))
-  #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001859384286")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "5925688295:AAFjVjgfg_m1f4Wd6B3AaqBAm8vFkH31guk")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Tamilan_Rocks1_Bot")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001884285982"))
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001859384286")
 
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6699193519:AAEDwcIK_PeHDyohxMrLVZcP3sAnBlVb8ik")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "QL_Hollywood_Dubbed_Movies_Bot")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002025122235"))
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001679142047")
+  #BOT_TOKEN = os.environ.get("BOT_TOKEN", "6699193519:AAEDwcIK_PeHDyohxMrLVZcP3sAnBlVb8ik")
+  #BOT_USERNAME = os.environ.get("BOT_USERNAME", "QL_Hollywood_Dubbed_Movies_Bot")
+  #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002025122235"))
+  #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001679142047")
   
   #BOT_TOKEN = os.environ.get("BOT_TOKEN", "6529788493:AAFp49RQQTHhv1P3DMxQUMMZUAzvQeWDkP0")
   #BOT_USERNAME = os.environ.get("BOT_USERNAME", "QTL_Series_Bot")
