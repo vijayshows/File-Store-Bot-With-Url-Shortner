d60190bc8a28f66c863b09e2c212ba3b68ba4bb9
mrhyper178 <153428922+mrhyper178@users.noreply.github.com>
2024-04-20 10:08:39 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,10 +9,10 @@ class Config(object):
   #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002087036746"))
   #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001679142047")
 
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7170082838:AAHS8JwuupFmSzmTijvahIIoA-cUwU5u9EA")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "QL_MoviesBot")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001924724963"))
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002025747602")
+  #BOT_TOKEN = os.environ.get("BOT_TOKEN", "7170082838:AAHS8JwuupFmSzmTijvahIIoA-cUwU5u9EA")
+  #BOT_USERNAME = os.environ.get("BOT_USERNAME", "QL_MoviesBot")
+  #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001924724963"))
+  #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002025747602")
   
   #BOT_TOKEN = os.environ.get("BOT_TOKEN", "7127431671:AAHTKHZEE61Tzwf1ijbYmzt_a8hEFn8J-VY")
   #BOT_USERNAME = os.environ.get("BOT_USERNAME", "Tamilan_Rocks5_Bot")
@@ -34,10 +34,10 @@ class Config(object):
   #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002035253761"))
   #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002028128566")
 
-  #BOT_TOKEN = os.environ.get("BOT_TOKEN", "5780282761:AAG9vcgAVK__K1btiy212Ao-9E_rlpelADw")
-  #BOT_USERNAME = os.environ.get("BOT_USERNAME", "Vijay_Tv_SerialsBot")
-  #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002117688872"))
-  #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001728131839")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "5780282761:AAG9vcgAVK__K1btiy212Ao-9E_rlpelADw")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Vijay_Tv_SerialsBot")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002117688872"))
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001728131839")
 
   #BOT_TOKEN = os.environ.get("BOT_TOKEN", "6885403506:AAEC0alu3bCjcCgzIMGqk6rG7540vmCkqP0")
   #BOT_USERNAME = os.environ.get("BOT_USERNAME", "AdultOnly18_Bot")
