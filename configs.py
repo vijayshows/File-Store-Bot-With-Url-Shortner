d98ac4f50d6466706ec2a1b61fc904f56e2ae162
mrhyper178 <153428922+mrhyper178@users.noreply.github.com>
2024-04-26 19:39:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -34,7 +34,7 @@ class Config(object):
   #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002035253761"))
   #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002028128566")
 
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "5780282761:AAG9vcgAVK__K1btiy212Ao-9E_rlpelADw")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "Vijay_Tv_SerialsBot")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002117688872"))
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001728131839")
